5d7b33087dc94e6652050807b0e557cd6808d37e
camila314 <47485054+camila314@users.noreply.github.com>
2024-10-26 13:12:27 -0500

    firefox extension

---
+++ ext/main.js
@@ -0,0 +1,11 @@
+browser.contextMenus.create({
+  id: "search-review",
+  title: "Search Reviews for '%s'",
+  contexts: ["selection"],
+});
+
+browser.contextMenus.onClicked.addListener((info, tab) => {
+	if (info.menuItemId === "search-review") {
+	    browser.tabs.create({ url: `http://localhost:6068/${encodeURIComponent(info.selectionText)}` });
+	}
+});
